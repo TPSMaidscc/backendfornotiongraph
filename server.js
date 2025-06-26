112fb9e8157dd8f10aec305cc9e9b232f1c680ee
TPSMaidscc <tpsmaidscc@gmail.com>
2025-06-26 16:48:49 +0300

    message

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const OpenAI = require('openai');
 // Initialize OpenAI
 const openai = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY 
+  apiKey: ''
 });
 
 // Firebase Admin SDK
